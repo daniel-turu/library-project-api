b5b54896bfce9062bb39655f8cc871ab7d63fdea
daniel-turu <danielturu30@gmail.com>
2023-02-07 22:52:39 +0100

    static

--- library_project/urls.py
+++ library_project/urls.py
@@ -13,6 +13,7 @@ Including another URLconfx
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import (
@@ -28,3 +29,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
